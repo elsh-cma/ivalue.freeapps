dfda07fef4f2e46dabea97cb69e1466969001d64
Yossef Elsherif <elsherif_1988@yahoo.com>
2018-08-18 13:26:36 +0200

    Edit website

--- hr_contract_allowance/__manifest__.py
+++ hr_contract_allowance/__manifest__.py
@@ -10,8 +10,8 @@
     """,
 
     'author': "I VALUE solutions",
-    'website': "www.ivalue-s.com",
-	'email': "info@ivalue-s.com",
+    'website': "www.odoo.com",
+	'email': "info@odoo.com",
     'license': "OPL-1",
 
     # Categories can be used to filter modules in modules listing
