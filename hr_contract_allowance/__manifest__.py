8e9625fa3e5addc58c3703df04a1fc0cb41cca28
Yossef Elsherif <elsherif_1988@yahoo.com>
2018-08-18 13:17:45 +0200

    First upload

---
+++ hr_contract_allowance/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee HR Allowances",
+
+    'summary': """
+        Allowance In Contract""",
+
+    'description': """
+        Allowance In Contract
+    """,
+
+    'author': "I VALUE solutions",
+    'website': "www.ivalue-s.com",
+	'email': "info@ivalue-s.com",
+    'license': "OPL-1",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Hr',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr','hr_contract'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+}
