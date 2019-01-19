d82968e9296474eaa01f3b67abd3f213b524f456
Yossef Elsherif <elsherif_1988@yahoo.com>
2019-01-19 13:51:09 +0200

    clear 11 version

--- hr_contract_salary_component/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class HrContract(models.Model):
-    _inherit = 'hr.contract'
-
-    basic = fields.Integer('Basic')
-    variable = fields.Integer('Variable')
